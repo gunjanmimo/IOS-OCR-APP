93a4494abcc832b87cd9b963ef265a4a1c711b33
gunjanmimo <gunjan.mtbpaul@gmail.com>
2021-07-02 22:52:18 +0530

    project init

---
+++ read-image/read-image/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  read-image
+//
+//  Created by Gunjan  Paul on 02/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ read-image/read-image/read_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  read_imageApp.swift
+//  read-image
+//
+//  Created by Gunjan  Paul on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct read_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
